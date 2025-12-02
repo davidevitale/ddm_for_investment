85d045dda92c339c0476d4784dbab8733cc14177
Alessiaaleale <106628136+Alessiaaleale@users.noreply.github.com>
2025-12-02 01:07:57 +0100

    Update feature_engineer.py

--- feature_engineer.py
+++ feature_engineer.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class FeatureEngineer:
     @staticmethod
-    def add_features(df: pd.DataFrame, sma_n: int, span_n: int) -> pd.DataFrame:
+    def add_features(df: pd.DataFrame, sma_n: int, span_n: int, main_ticker: str) -> pd.DataFrame:
         """
         Add technical features to the DataFrame.
         """
@@ -15,10 +15,10 @@ class FeatureEngineer:
         # Standard Deviation of 'Difference'
         out_df["STDEV"] = out_df["Difference"].rolling(sma_n).std(ddof=1).replace(0, np.nan)
         
-        # Z-Score calculation
+        # Z-Score
         out_df['z_score'] = (out_df['Difference'] - out_df['SMA']) / out_df['STDEV']
         
-        # Exponential Moving Average of SPY Volume
-        out_df['EMA'] = out_df['SPY_Volume'].ewm(span=span_n, adjust=False).mean()
+        # Exponential Moving Average of main ticker Volume
+        out_df['EMA'] = out_df[f"{main_ticker}_Volume"].ewm(span=span_n, adjust=False).mean()
         
         return out_df
